748677a0077cf59a109e0daf6094d1606ec2af99
ExcaliburSims <ximeonzilu@gmail.com>
2022-05-09 03:21:48 +0100

    feat: create search component

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Search= () => {
+	return (
+ 		<>
+			<h1>sims</h1>
+			<div class="search-wrapper">
+      <div class="fa fa-search"></div>
+      <input type="text" class="input" placeholder="Search...." />
+      <div class="fa fa-times close"></div>
+    </div>
+		</>
+	)
+}
+
+export default Search
