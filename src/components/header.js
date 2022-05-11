d637c22add794c2a6ed40ce84080bd92a66ad35e
ExcaliburSims <ximeonzilu@gmail.com>
2022-05-11 02:54:14 +0100

    feat: customize header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { MdAddAlert } from 'react-icons/md';
 import styles from '../styles/header.module.scss';
 import Search from './Search';
 
 const header = () => (
   <div className={styles.container}>
     <div className={styles.logo}>Bendanga</div>
-    <div className={styles.searchbar}>
+    <div className={styles.search}>
       <Search />
+      <MdAddAlert className={styles.icon} />
     </div>
   </div>
 );
