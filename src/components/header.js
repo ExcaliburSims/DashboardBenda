c9883df838f65d93d922e105f1640cfda3f91843
ExcaliburSims <ximeonzilu@gmail.com>
2022-05-11 02:10:45 +0100

    feat: add search component on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from '../styles/header.module.scss';
+import Search from './Search';
 
 const header = () => (
   <div className={styles.container}>
     <div className={styles.logo}>Bendanga</div>
-    <div className={styles.searchbar}>Login</div>
+    <div className={styles.searchbar}>
+      <Search />
+    </div>
   </div>
 );
 
