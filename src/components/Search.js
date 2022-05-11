ae5eacdab4d353daa4d44e6717dd26f58fca0cb6
ExcaliburSims <ximeonzilu@gmail.com>
2022-05-11 02:26:11 +0100

    feat: customize searchbar

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,6 @@ import styles from '../styles/search.module.scss';
 
 function Search() {
   return (
-    // eslint-disable-next-line react/jsx-filename-extension
     <div className={styles.search_wrapper}>
       <MdSearch />
       <input type="text" className={styles.input} placeholder="Search...." />
