da9c1103c2ef3b2544dc12712a9fb643e460bc57
ExcaliburSims <ximeonzilu@gmail.com>
2022-05-12 13:41:47 +0100

    feat: change icon of notification

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MdAddAlert } from 'react-icons/md';
+import { MdNotifications } from 'react-icons/md';
 import styles from '../styles/header.module.scss';
 import Search from './Search';
 
@@ -8,7 +8,7 @@ const header = () => (
     <div className={styles.logo}>Bendanga</div>
     <div className={styles.search}>
       <Search />
-      <MdAddAlert className={styles.icon} />
+      <MdNotifications className={styles.icon} />
     </div>
   </div>
 );
