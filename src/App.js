d7164648d3b8eab185559ae3aeeda10f28bb6534
ExcaliburSims <ximeonzilu@gmail.com>
2022-05-12 13:40:58 +0100

    feat: add sidebar to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="container">
       <div>
         <Header />
+        <Sidebar />
       </div>
     </div>
   );
