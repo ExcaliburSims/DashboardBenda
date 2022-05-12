4559c93f23d56987eeceed881e869ff2ca0f34ac
ExcaliburSims <ximeonzilu@gmail.com>
2022-05-12 14:46:07 +0100

    feat: add another options

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
-  MdPerson, MdOutlineVisibility, MdOutlinePeopleAlt, MdBuild, MdLeaderboard,
+  // eslint-disable-next-line max-len
+  MdPerson, MdOutlineVisibility, MdOutlinePeopleAlt, MdBuild, MdLeaderboard, MdNotificationImportant, MdGpsFixed,
 } from 'react-icons/md';
 import styles from '../styles/sidebar.module.scss';
 
@@ -9,11 +10,11 @@ function Sidebar() {
     <div className={styles.container}>
       <div className={styles.search}>
         <MdOutlineVisibility className={styles.icon} />
-        <h5>overview</h5>
+        <h5>Overview</h5>
       </div>
       <div className={styles.search}>
         <MdPerson className={styles.icon} />
-        <h5>users</h5>
+        <h5>Users</h5>
       </div>
       <div className={styles.search}>
         <MdOutlinePeopleAlt className={styles.icon} />
@@ -24,8 +25,16 @@ function Sidebar() {
         <h5>Garages</h5>
       </div>
       <div className={styles.search}>
+        <MdNotificationImportant className={styles.icon} />
+        <h5>Alertes</h5>
+      </div>
+      <div className={styles.search}>
+        <MdGpsFixed className={styles.icon} />
+        <h5>Cartes</h5>
+      </div>
+      <div className={styles.search}>
         <MdLeaderboard className={styles.icon} />
-        <h5>statistiques</h5>
+        <h5>Stats</h5>
       </div>
     </div>
   );
