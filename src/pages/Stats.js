7c46c03d633cfcae70632643df339db14b36b424
ExcaliburSims <ximeonzilu@gmail.com>
2022-05-17 07:28:48 +0100

    feat: customize stats

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/header';
+import styles from '../styles/stats.module.scss';
 
 function Stats() {
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
-      <Sidebar />
-      <h3>Stats</h3>
+      <div className={styles.sub_container}>
+        <Sidebar />
+        <h3>Stats</h3>
+      </div>
     </div>
   );
 }
